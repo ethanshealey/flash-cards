1db093a37427f4075b06d6191524ba651b72f842
ethanshealey <ethan.shealey@gmail.com>
2023-10-26 21:11:51 -0400

    add to github

--- src/helpers/base64.ts
+++ src/helpers/base64.ts
@@ -1,5 +1,4 @@
 import { encode as b64_encode, decode as b64_decode } from 'base-64';
-import utf8 from 'utf8'
 
 const encode = (str: string) => b64_encode(encodeURIComponent(str))
 const decode = (str: string) => decodeURIComponent(b64_decode(str))
