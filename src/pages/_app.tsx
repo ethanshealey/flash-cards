d5a361498832a9cb98145136f32517aadc35742e
ethanshealey <ethan.shealey@gmail.com>
2023-10-29 18:04:43 -0400

    fix font issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,6 +46,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name="description" content="A flash card app for all your studying needs!" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;200;300;400;500;600;700;800;900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200;0,6..12,300;0,6..12,400;0,6..12,500;0,6..12,600;0,6..12,700;0,6..12,800;0,6..12,900;0,6..12,1000;1,6..12,200;1,6..12,300;1,6..12,400;1,6..12,500;1,6..12,600;1,6..12,700;1,6..12,800;1,6..12,900;1,6..12,1000&display=swap" />
       </Head>
       { isLoading ? <div>Loading...</div> : <Component {...pageProps} user={user} /> }
     </>
